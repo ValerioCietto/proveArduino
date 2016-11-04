c1df1a9d4c159146722e3c7fd5d5afb835b811b8
Valerio Cietto <valerio_ciet@hotmail.it>
2016-11-04 17:46:42 +0100

    Update ESP8266_sd_WiFi-AP_log.ino

--- ESP8266_sd_WiFi-AP_log.ino
+++ ESP8266_sd_WiFi-AP_log.ino
@@ -19,13 +19,17 @@ ESP8266WebServer server(80);
  */
 
  
-
+int sd_true=0;
 const int MPU_addr=0x68;  // I2C address of the MPU-6050
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 double tmp_double;
 int16_t AcX1,AcY1,AcZ1,Tmp1,GyX1,GyY1,GyZ1;
 int16_t AcX2,AcY2,AcZ2,Tmp2,GyX2,GyY2,GyZ2;
 int16_t AcX3,AcY3,AcZ3,Tmp3,GyX3,GyY3,GyZ3;
+
+int acx_buffer[10];
+int acy_buffer[10];
+int acz_buffer[10];
 int maxNumEntries=3600;
 File data;
 
@@ -40,8 +44,9 @@ void setup(){
   Serial.print("Initializing SD card...");
   if (!SD.begin(D8)) {//D8
     Serial.println("initialization failed!");
-    return;
+    
   }
+  else{
   Serial.println("initialization done.");
   // Check to see if the file exists:
   if (SD.exists("data.txt")) {
@@ -49,7 +54,8 @@ void setup(){
     data = SD.open("data.txt", FILE_WRITE);
     data.println("x,y,z,t,r,p,w;");
   }
-
+  }
+  Serial.print("sd card done");
   Serial.print("Configuring access point...");
   /* You can remove the password parameter if you want the AP to be open. */
   WiFi.softAP(ssid, password);
@@ -61,7 +67,7 @@ void setup(){
   server.begin();
   Serial.println("HTTP server started");
   
-  
+  server.on("/test.svg",drawGraph);
   
   server.on("/dati", handleDati);
 
@@ -69,9 +75,10 @@ void setup(){
 }
 void handleDati(){
   int val = analogRead(0);
-  char testo[200];
-  sprintf(testo,"%d,%d,%d,%d,%d,%d", AcX,AcY,AcZ,GyX,GyY,GyZ);
-  
+  char testo[2000];
+  for(int i =0;i<10;i++){
+    sprintf(testo,"%d,%d,%d\\n", acx_buffer[i],acy_buffer[i],acz_buffer[i]);
+  }
   server.send(200, "text/html", testo);
   
 }
@@ -80,27 +87,63 @@ void handleRoot() {
   int sec = millis() / 1000;
   int min = sec / 60;
   int hr = min / 60;
-
+  Serial.println("serving page");
   snprintf ( temp, 400,
 
   "<html>\
     <head>\
-      <meta http-equiv='refresh' content='5'/>\
+      <meta http-equiv='refresh' content='10'/>\
       <title>ESP8266 Demo</title>\
       <style>\
         body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
       </style>\
     </head>\
     <body>\
-      <h1>Hello from ESP8266!</h1>\
+      <h1>Ciao dall'ESP8266!</h1>\
       <p>Uptime: %02d:%02d:%02d</p>\
-      <img src=\"/test.svg\" />\
+      <script>\
+      var acx = []\
+      var acy = []\
+      var acz = []\
+      
+      </script>\
+      Tutto funziona ok\
     </body>\
   </html>",
 
     hr, min % 60, sec % 60
   );
   server.send ( 200, "text/html", temp );
+}
+
+void drawGraph() {
+  String out = "";
+  char temp[100];
+  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"800\" height=\"450\">\n";
+  out += "<rect width=\"800\" height=\"450\" fill=\"rgb(250, 250, 250)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
+  out += "<g stroke=\"red\">\n";
+  int y = rand() % 130;
+  for (int x = 10; x < 390; x+= 10) {
+    int y2 = rand() % 130;
+    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
+    out += temp;
+    y = y2;
+  }
+  out += "</g>\n";
+
+  out += "<g stroke=\"blue\">\n";
+  int z = rand() % 130;
+  for (int x = 10; x < 390; x+= 10) {
+    int y2 = rand() % 130;
+    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - z, x + 10, 140 - y2);
+    out += temp;
+    y = y2;
+  }
+  out += "</g>\n";
+  
+  out += "</svg>\n";
+
+  server.send ( 200, "image/svg+xml", out);
 }
 
 void loop(){
@@ -117,10 +160,12 @@ void loop(){
   GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
-  
-  Serial.print("AcX(x) = "); Serial.print(AcX);
-  Serial.print(" | AcY(y) = "); Serial.print(AcY);
-  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  double cifosi = (atan2(AcX, AcZ))*90/1.63;
+  double scogliosi = ((atan2(AcY, AcZ))*90/1.63);
+  double rotazione = (atan2(AcX, AcY))*90/1.63;
+  Serial.print("cifosi = "); Serial.print(cifosi);
+  Serial.print(" | scogliosi = "); Serial.print(scogliosi);
+  Serial.print(" | rotazione = "); Serial.print(rotazione);
   tmp_double= (Tmp/340.00)+36.53;
   Serial.print(" | Tmp(t) = "); Serial.print(tmp_double);  //equation for temperature in degrees C from datasheet
   Serial.print(" | GyX(r) = "); Serial.print(GyX);
@@ -144,6 +189,7 @@ void loop(){
     // close the file:
     data.close();
     Serial.println("done.");
+    sd_true=1;
   } else {
     // if the file didn't open, print an error:
     Serial.println("error opening sensorData.txt");
