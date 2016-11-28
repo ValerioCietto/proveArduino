04dc43455bcbc854bedb581491afdde5357ea189
Valerio Cietto <valerio_ciet@hotmail.it>
2016-11-28 17:30:34 +0100

    added sd status page, pagina /analisi
    Nella pagina root indica lo stato della sd card.
    Aggiunta pagina /analisi che dice grado di cifosi.

--- AsdShirt.cpp
+++ AsdShirt.cpp
@@ -8,7 +8,6 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
-
 //TODO
 /*
 -polling NTP// alternative js tells the nodmcu the time
@@ -28,36 +27,44 @@
 
 void handleRoot();
 void handleDati();
+void handleAnalysis();
 void handleClient();
 void drawGraph();
 void getInfo();
 int sdSetup();
 int wifiSetup();
+int wireSetup();
+void readT12sensor();
+void readC7sensor();
+void readShoulderSensor();
 
 /* Set these to your desired credentials. */
 const char *ssid = "asdShirt";
 const char *password = "12345678";
 
-
-
-
-
 ESP8266WebServer server(80);
-/* Just a little test message.  Go to http://192.168.4.1 in a web browser
- * connected to this access point to see it.
- */
+
 MDNSResponder mdns;
 
 int sd_true=0;
-const int primary_MPU_address=0x68;  // yellow AD0
-const int secondary_MPU_address=0x69; //violet AD0
+const int primary_MPU_address=0x68;
+const int secondary_MPU_address=0x69;
 
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 double tmp_double;
 int16_t AcX1,AcY1,AcZ1,Tmp1,GyX1,GyY1,GyZ1;
 double tmp_double1;
 int16_t AcX2,AcY2,AcZ2,Tmp2,GyX2,GyY2,GyZ2;
+double tmp_double2;
 int16_t AcX3,AcY3,AcZ3,Tmp3,GyX3,GyY3,GyZ3;
+
+double cifosi = 0;
+double scogliosi = 0;
+double rotazione = 0;
+
+double cifosi1 = 0;
+double scogliosi1 = 0;
+double rotazione1 = 0;
 
 int acx_buffer[10];
 int index_acx=0;
@@ -73,33 +80,53 @@ int index_acy1=0;
 int acz_buffer1[10];
 int index_acz1=0;
 
+
+
 int maxNumEntries=3600;
 File data;
+File network_config;
+String filename="data.csv";
+
+/*
+red vcc
+brown gnd
+orange scl
+yellow sda
+*/
 
 void setup(){
-  Wire.begin();
-  Wire.beginTransmission(primary_MPU_address);
-  Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
-  Wire.endTransmission(true);
+
   Serial.begin(115200);
-
-  sdSetup();
-
   wifiSetup();
 
+  delay(1000);
+
+  sdSetup();
+  wireSetup();
 
 
   server.on("/test.svg",drawGraph);
 
   server.on("/dati", handleDati);
+  server.on("/analisi", handleAnalysis);
   server.on("/info", getInfo);
   server.on("/", handleRoot);
   server.begin();
   Serial.println("HTTP server started");
-
   //NTP poll to know how to name the file
 
+}
+int wireSetup(){
+  Wire.begin(D1,D2);
+  Wire.beginTransmission(primary_MPU_address);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+
+  Wire.beginTransmission(secondary_MPU_address);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
 
 
 }
@@ -119,7 +146,7 @@ int wifiSetup(){
       WiFi.begin("Wifi", "wifidicasa1.");
 
       while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
+      delay(1000);
       Serial.print(".");
       }
       Serial.println("");
@@ -166,19 +193,15 @@ int sdSetup(){
   Serial.print("Initializing SD card...");
   if (!SD.begin(D8)) {//D8
     Serial.println("initialization failed!");
-
+    return 0;
   }
   else{
   Serial.println("initialization done.");
   // Check to see if the file exists:
-  if (SD.exists("data.csv")) {
-    SD.remove("data.csv");
 
-    Serial.println("data.csv existed.");
-    data = SD.open("data.csv", FILE_WRITE);
-    data.println("x,y,z,t,r,p,w;");
   }
-  }
+
+
   Serial.print("sd card done");
 
 }
@@ -195,46 +218,34 @@ void getInfo(){
 void handleDati(){
   int val = analogRead(0);
   char testo[2000];
-  for(int i =0;i<10;i++){
-    sprintf(testo,"%d,%d,%d\\n", acx_buffer[i],acy_buffer[i],acz_buffer[i]);
+  for(int i =0;i<1;i++){
+    sprintf(testo,"%d,%d,%d,%d", AcX, AcY, AcX1, AcY1);
+  }
+  Serial.print("dati forniti :) ");
+  server.send(200, "text/html", testo);
+
+}
+void handleAnalysis(){
+  int val = analogRead(0);
+  char testo[2000];
+  for(int i =0;i<1;i++){
+    sprintf(testo,"%d,%d,%d,%d,%d", cifosi, scogliosi, cifosi1, scogliosi1, cifosi1-cifosi);
   }
   Serial.print("dati forniti :) ");
   server.send(200, "text/html", testo);
 
 }
 void handleRoot() {
-  char temp[400];
+
   int sec = millis() / 1000;
   int min = sec / 60;
   int hr = min / 60;
-  Serial.println("serving page");
-  /*
-  snprintf ( temp, 400,
-
-  "<html>\
-    <head>\
-      <meta http-equiv='refresh' content='10'/>\
-      <title>ESP8266 Demo</title>\
-      <style>\
-        body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-      </style>\
-    </head>\
-    <body>\
-      <h1>Ciao dall'ESP8266!</h1>\
-      <p>Uptime: %02d:%02d:%02d</p>\
-      <script>\
-      var acx = []\
-      var acy = []\
-      var acz = []\
-
-      </script>\
-      Tutto funziona ok\
-    </body>\
-  </html>",
-
-    hr, min % 60, sec % 60
-  );*/
-  server.send ( 200, "text/html", temp );
+  char testo[2000];
+  for(int i =0;i<1;i++){
+    sprintf(testo,"Attivo da %d:%d:%d Stream su sd (1 funziona, 0 non funziona:)%d", hr, min, sec, sd_true);
+  }
+  Serial.println("diagnostica fornita.");
+  server.send(200, "text/html", testo);
 }
 
 void drawGraph() {
@@ -261,11 +272,24 @@ void drawGraph() {
   server.send ( 200, "image/svg+xml", out);
 }
 
+void readT12sensor(){
+
+}
+void readC7sensor(){
+
+}
+void readShoulderSensor(){
+
+
+}
+
+
 int serverTimer=0;
 void loop(){
-  if(serverTimer==50){
+  if(serverTimer==10){
   serverTimer=0;
 
+  void readT12sensor();
 
   Wire.beginTransmission(primary_MPU_address);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
@@ -278,47 +302,62 @@ void loop(){
   GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  void readC7sensor();
+  Wire.beginTransmission(secondary_MPU_address);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(secondary_MPU_address,14,true);  // request a total of 14 registers
+  AcX1=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  AcY1=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ1=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp1=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX1=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY1=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ1=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  //void readShoulderSensor();
 
-  acx_buffer[index_acx]=AcX;
-  if(index_acx==9){
-    index_acx=0;
-  }else{
-    index_acx=index_acx+1;
-  }
-
-  Serial.println(index_acx);
-  Serial.println(acx_buffer[index_acx]);
-  for(int i=0;i<10;i++){
-    Serial.print(acx_buffer[i]);
-    Serial.print(",");
-  }
-  Serial.println(".");
-  for(int i=0;i<index_acx;i++){
-    Serial.print(acx_buffer[i]);
-    Serial.print(",");
-  }
-  for(int i=index_acx;i<10;i++){
-    Serial.print(acx_buffer[i]);
-    Serial.print(",");
-  }
-
-  Serial.println("");
-
-  double cifosi = (atan2(AcX, AcZ))*90/1.63;
-  double scogliosi = ((atan2(AcY, AcZ))*90/1.63);
-  double rotazione = (atan2(AcX, AcY))*90/1.63;
-  Serial.print("cifosi = "); Serial.print(cifosi);
-  Serial.print(" | scogliosi = "); Serial.print(scogliosi);
-  Serial.print(" | rotazione = "); Serial.print(rotazione);
+  cifosi = (atan2(AcX, AcZ))*90/1.6384;
+  scogliosi = ((atan2(AcY, AcZ))*90/1.6384);
+  rotazione = (atan2(AcX, AcY))*90/1.6384;
+  //Serial.print(","); Serial.print(AcX);
+  //Serial.print(","); Serial.print(AcY);
+  //Serial.print(","); Serial.print(AcZ);
   tmp_double= (Tmp/340.00)+36.53;
-  Serial.print(" | Tmp(t) = "); Serial.print(tmp_double);  //equation for temperature in degrees C from datasheet
-  Serial.print(" | GyX(r) = "); Serial.print(GyX);
-  Serial.print(" | GyY(p) = "); Serial.print(GyY);
-  Serial.print(" | GyZ(w) = "); Serial.println(GyZ);
+
+  Serial.print(","); Serial.print(cifosi);
+  Serial.print(","); Serial.print(scogliosi);
+  //Serial.print(" | Tmp(t) = "); Serial.print(tmp_double);  //equation for temperature in degrees C from datasheet
+  //Serial.print(","); Serial.print(GyX);
+  //Serial.print(","); Serial.print(GyY);
+  //Serial.print(","); Serial.print(GyZ);
+
+  cifosi1 = (atan2(AcX1, AcZ1))*90/1.6384;
+  scogliosi1 = ((atan2(AcY1, AcZ1))*90/1.6384);
+  rotazione1 = (atan2(AcX1, AcY1))*90/1.6384;
+  //Serial.print(","); Serial.print(AcX1);
+  //Serial.print(","); Serial.print(AcY1);
+  //Serial.print(","); Serial.print(AcZ1);
+  tmp_double= (Tmp1/340.00)+36.53;
+  Serial.print(","); Serial.print(cifosi1);
+  Serial.print(","); Serial.print(scogliosi1);
+  //Serial.print(" | Tmp1(t) = "); Serial.print(tmp_double);  //equation for temperature in degrees C from datasheet
+  //Serial.print(","); Serial.print(GyX1);
+  //Serial.print(","); Serial.print(GyY1);
+  //Serial.print(","); Serial.println(GyZ1);
+
+
+
+  Serial.print(","); Serial.print(cifosi1-cifosi);
+
+
   if(AcX<14000){
-    Serial.print("vibra");
+    //Serial.print("vibra");
   }
-  data = SD.open("data.csv", FILE_WRITE);
+
+ Serial.println();
+
+
+  data = SD.open(filename, FILE_WRITE);
 
   // if the file opened okay, write to it:
   if (data) {
@@ -329,19 +368,27 @@ void loop(){
     data.print(tmp_double);data.print(",");
     data.print(GyX);data.print(",");
     data.print(GyY);data.print(",");
-    data.print(GyZ);data.println(";");
+    data.print(GyZ);data.print(",");
+
+    data.print(AcX1);data.print(",");
+    data.print(AcY1);data.print(",");
+    data.print(AcZ1);data.print(",");
+    data.print(tmp_double);data.print(",");
+    data.print(GyX1);data.print(",");
+    data.print(GyY1);data.print(",");
+    data.print(GyZ1);data.println(",;");
     // close the file:
     data.close();
     Serial.println("done.");
     sd_true=1;
   } else {
     // if the file didn't open, print an error:
-    Serial.println("error opening data.txt");
+    //Serial.println("error opening data.txt");
   }
 
   }else{
     server.handleClient();
     serverTimer+=1;
   }
-  delay(20);
+  delay(200);
 }
